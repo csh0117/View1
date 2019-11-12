c2c9a4da8eabddea2ac9c0468891b45a4ed8fc4c
csh0117 <ckdbrud8678@gmail.com>
2019-11-12 22:15:21 +0900

    3교시 끝

--- app/src/main/java/com/android/view1/MainActivity.java
+++ app/src/main/java/com/android/view1/MainActivity.java
@@ -4,29 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+
     Button btn1;
     Button btn2;
     Button btn3;
-    EditText editText=null;
+
+    EditText editText;
+
+    //3교시-입력된 텍스트를 하단에 출력
+    TextView resultText=null;
+    Button inputBtn=null;
+    String rsStr=null; // EditText 입력될 문자열을 저장 -> TextView(resultText)
+
+    String[] strArr = {"android1", "andriod2", "andriod3", "IOS1", "IOS2", "IOS3"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        AutoCompleteTextView autoCompleteTextView = (AutoCompleteTextView)findViewById(R.id.autoText);
+
+        autoCompleteTextView.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,strArr));
+
+
         textView = (TextView)findViewById(R.id.text1);
         btn1 = (Button)findViewById(R.id.btn1);
         btn2 = (Button)findViewById(R.id.btn2);
         btn3 = (Button)findViewById(R.id.btn3);
         editText = (EditText)findViewById(R.id.edit1);
+
+        resultText = (TextView)findViewById(R.id.resultText);
+        inputBtn = (Button)findViewById(R.id.inputBtn);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,6 +80,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        inputBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rsStr = editText.getText().toString(); //EditText에 입력되어 있는 문자열을 저장
+                Toast.makeText(getApplicationContext(),rsStr,Toast.LENGTH_SHORT).show();
+                resultText.setText(rsStr);
+            }
+        });
     }
 }
